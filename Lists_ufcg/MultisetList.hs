b5fe3c8167c0266d64fc49426d7b3580c49e2a6e
Gabriel-Emannuel <gabriel.emannuel.gama.andrade@gmail.com>
2024-08-09 00:11:35 -0300

    implementar MultiSetList

--- Lists_ufcg/MultisetList.hs
+++ Lists_ufcg/MultisetList.hs
@@ -15,11 +15,11 @@ removeMultiSetList elem ((x, i) : xs)
     | x == elem && i > 1 = (x, i - 1) : xs
     | otherwise = xs
 
-search :: (Eq a) => a -> [(a, Int)] -> (a, Int)
-search t [] = (t, 0)
-search t ((a, i) : xs) 
+searchMultiList :: (Eq a) => a -> [(a, Int)] -> (a, Int)
+searchMultiList t [] = (t, 0)
+searchMultiList t ((a, i) : xs) 
     | t == a    = (a, i)
-    | otherwise = search t xs
+    | otherwise = searchMultiList t xs
 
 unionMultiSetList :: (Eq a) => [(a, Int)] -> [(a, Int)] -> [(a, Int)]
 unionMultiSetList [] bag2 = bag2
@@ -27,7 +27,7 @@ unionMultiSetList ((a, i): xs) bag2
     | x /= 0    = (a, i + x) : (unionMultiSetList xs . filter (\(z, _) -> z /= a)) bag2
     | otherwise = (a, i) : unionMultiSetList xs bag2
     where 
-        (t, x) = search a bag2
+        (t, x) = searchMultiList a bag2
 
 intersection :: (Eq a) => [(a, Int)] -> [(a, Int)] -> [(a, Int)]
 intersection [] bag2 = []
@@ -35,7 +35,7 @@ intersection ( ( t, i ) : xs ) bag2
     | min y i /= 0 = (t, min y i) : intersection xs bag2
     | otherwise    = (t, i) : intersection xs bag2   
         where 
-            (a, y) = search t bag2
+            (a, y) = searchMultiList t bag2
 
 minus :: (Eq a) => [(a, Int)] -> [(a, Int)] -> [(a, Int)]
 minus [] bag2 = []
@@ -44,20 +44,20 @@ minus ((t, i) : xs) bag2
     | y > i     = minus xs bag2
     | otherwise = (t, i) : minus xs bag2
     where
-        (a, y) = search t bag2
+        (a, y) = searchMultiList t bag2
 
 inclusion :: (Eq a) => [(a, Int)] -> [(a, Int)] -> Bool
 inclusion [] bag2 = True
 inclusion ((a, i) : xs) bag2 = y >= i && inclusion xs bag2
     where
-        (t, y) = search a bag2
+        (t, y) = searchMultiList a bag2
 
 sumMultiSetList :: (Eq a) => [(a, Int)] -> [(a, Int)] -> [(a, Int)]
 sumMultiSetList [] bag2 = bag2
 sumMultiSetList ((a, i) : xs) bag2 = 
     (a, i + y) : (sumMultiSetList xs . filter (\(z, _) -> z /= a )) bag2
     where
-        (_, y) = search a bag2
+        (_, y) = searchMultiList a bag2
 
 sizeMultiSetList :: [(a, Int)] -> Int
 sizeMultiSetList [] = 0
